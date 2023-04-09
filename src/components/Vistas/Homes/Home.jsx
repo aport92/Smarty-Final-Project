9e6ebbc30dcbc802a1040d086c79e50dda3476a9
Alba Portillo <portilloalba92@gmail.com>
2023-04-09 03:09:53 -0600

    changes

--- src/components/Vistas/Homes/Home.jsx
+++ src/components/Vistas/Homes/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
       <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-        <img className="object-cover object-center rounded" alt="img" to={"./src/images/Typing.gif"} />
+        <img alt="" src={"../images/Typing.gif"} />
       </div>
 
       <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
--- src/components/Vistas/Navbar.jsx
+++ src/components/Vistas/Navbar.jsx
@@ -1,23 +1,22 @@
 import { Link } from 'react-router-dom'
-
+import './Navbar.css'
 
 export const Navbar = () => {
-    return(
-<header className="text-gray-600 body-font">
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <Link to="/Home" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <span className="ml-3 text-xl">Smarty</span>
-    </Link>
-    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <Link to="/Smarty" className="mr-5 hover:text-gray-900">Qué es Smarty</Link>
-      <Link to="/" className="mr-5 hover:text-gray-900">Materias</Link>
-      <Link to="/" className="mr-5 hover:text-gray-900">Registro</Link>
-    </nav>
-    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Inicia Sesión
-      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-      </svg>
-    </button>
-  </div>
-</header>
-    )
+  return(
+    <div className="wrapper">
+      <nav>
+         <div className="flex-shrink-0 flex items-center">
+          <img className="hidden lg:block h-8 w-auto" src={"src/images/smarty-favicon.png"} alt="Smarty logo" />
+             <label for="toggle"><i className='material-icons'>Menu</i></label>
+            <div className='menu'>
+            <ul>
+              <li><Link to="/Home">Inicio</Link></li>
+              <li><Link to="/Smarty">Qué es Smarty</Link></li>
+              <li><Link to="/Materias">Materias</Link></li>
+              <li><Link to="/InicioSesion">Inicia Sesión</Link></li>
+            </ul>
+          </div>
+       </nav>
+    </div>
+)
 };
