e970e0a898051920e9fd69e2797298359a473e55
Alba Portillo <portilloalba92@gmail.com>
2023-04-09 00:43:57 -0600

    Update Router.jsx

--- src/components/ROUTER/Router.jsx
+++ src/components/ROUTER/Router.jsx
@@ -1,14 +1,13 @@
 import { Router, Routes, Route } from "react-router-dom";
-import { Navbar } from "../Navbar";
 import Home from "../Vistas/Homes/Home";
 
 export default function () {
   return (
     <Router>
       <Routes>
-
-<Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
       </Routes>
     </Router>
   );
 }
+
