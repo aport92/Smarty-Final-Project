460bccb919165870fee13183f98d0e9a4d91aab3
Alba Portillo <portilloalba92@gmail.com>
2023-04-09 03:14:24 -0600

    Update Navbar.jsx

--- src/components/Vistas/Navbar.jsx
+++ src/components/Vistas/Navbar.jsx
@@ -5,18 +5,19 @@ export const Navbar = () => {
   return(
     <div className="wrapper">
       <nav>
-         <div className="flex-shrink-0 flex items-center">
-          <img className="hidden lg:block h-8 w-auto" src={"src/images/smarty-favicon.png"} alt="Smarty logo" />
-             <label for="toggle"><i className='material-icons'>Menu</i></label>
-            <div className='menu'>
-            <ul>
-              <li><Link to="/Home">Inicio</Link></li>
-              <li><Link to="/Smarty">Qué es Smarty</Link></li>
-              <li><Link to="/Materias">Materias</Link></li>
-              <li><Link to="/InicioSesion">Inicia Sesión</Link></li>
-            </ul>
-          </div>
-       </nav>
+          <div className="flex-shrink-0 flex items-center">
+              <img className="hidden lg:block h-8 w-auto" src={"src/images/smarty-favicon.png"} alt="Smarty logo" />
+                <label for="toggle"><i className='material-icons'>Menu</i></label>
+                  <div className='menu'>
+                    <ul>
+                      <li><Link to="/Home">Inicio</Link></li>
+                      <li><Link to="/Smarty">Qué es Smarty</Link></li>
+                      <li><Link to="/Materias">Materias</Link></li>
+                      <li><Link to="/InicioSesion">Inicia Sesión</Link></li>
+                    </ul>
+                  </div>
+            </div>
+      </nav>
     </div>
 )
 };
