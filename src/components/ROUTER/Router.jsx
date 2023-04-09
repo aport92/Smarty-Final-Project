e34ea47fb1f2a3211717bbe4b9f060b5a38801c9
Nancy Ramírez <nancylissethramirezservellon@gmail.com>
2023-04-09 04:12:44 -0600

    ultimate

--- src/components/ROUTER/Router.jsx
+++ src/components/ROUTER/Router.jsx
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../Vistas/Homes/Home";
+import Smarty from "../Vistas/Smarty/Smarty";
 
 export default function () {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Smarty" element={<Smarty />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Vistas/Navbar.jsx
+++ src/components/Vistas/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 import logo from "../../images/smarty-home.png";
+import menu from "../../images/menu-burger-horizontal.svg"
 
 export const Navbar = () => {
   return(
@@ -10,10 +11,12 @@ export const Navbar = () => {
         <img className=" lg:block h-8 w-auto" src={logo} alt="Smarty logo" />
         </div>
               <input type="checkbox" name="" id="toggle"></input>
-                <label for="toggle"><i className='material-icons'>Menu</i></label>
+                <label for="toggle">
+                  <img src={menu} alt="" width="25px"/>
+                  </label>
                   <div className='menu'>
                     <ul>
-                      <li><Link to="/Home">Inicio</Link></li>
+                      <li><Link to="/">Inicio</Link></li>
                       <li><Link to="/Smarty">Qué es Smarty</Link></li>
                       <li><Link to="/Materias">Materias</Link></li>
                       <li><Link to="/InicioSesion">Inicia Sesión</Link></li>
