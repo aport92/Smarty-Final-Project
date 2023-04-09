f624adb21389a5dedf2f4d46d56c34f3473a120f
Alba Portillo <portilloalba92@gmail.com>
2023-04-09 15:38:13 -0600

    cambios

--- src/components/Vistas/Homes/Home.jsx
+++ src/components/Vistas/Homes/Home.jsx
@@ -2,7 +2,7 @@ import { React } from "react";
 import { Navbar } from "../Navbar";
 import Footer from "../Footer";
 import { Link } from "react-router-dom";
-import imgHome from "../../images/imgHome.gif";
+import imgHome from "../Homes/imgHome.gif";
 
 const Home = () => {
   return (
@@ -12,7 +12,7 @@ const Home = () => {
             <section className="text-gray-600 body-font">
               <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                  <img alt="" src={imgHome} width=""/>
+                <img src={imgHome} alt="" class="object-cover object-center rounded"/>
                   </div>
                   <section className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                     <h1 className="font-font1 sm:text-4xl text-3xl mb-4 font-medium text-mainblue"> ¡Aprender Siempre es genial!</h1>
