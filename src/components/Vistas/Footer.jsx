35889e69dffdbfbd15d85a20a723c6b5d76e5c70
Alba Portillo <portilloalba92@gmail.com>
2023-04-09 03:22:51 -0600

    Update Footer.jsx

--- src/components/Vistas/Footer.jsx
+++ src/components/Vistas/Footer.jsx
@@ -1,7 +1,7 @@
 
 import {React} from 'react';
 import { Link } from 'react-router-dom';
-
+import logo from "../../images/smarty-home.png";
 function Footer() {
 
     //{/*TODO JS */}
@@ -11,7 +11,7 @@ function Footer() {
 <footer className="text-gray-600 body-font">
   <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
     <Link className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-    <img className="hidden lg:block h-8 w-auto" src={"src/images/smarty-favicon.png"} alt="Smarty logo" />
+    <img className="hidden lg:block h-8 w-auto" src={logo} alt="Smarty logo" />
     </Link>
     <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 Smarty
     </p>
