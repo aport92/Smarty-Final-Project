189e06bb3b4a9a26508512d0b4ea6c6271c43c49
Nancy Ramírez <nancylissethramirezservellon@gmail.com>
2023-04-09 01:02:34 -0600

    Ultimos

--- src/components/Vistas/Homes/Home.jsx
+++ src/components/Vistas/Homes/Home.jsx
@@ -1,15 +1,14 @@
-import {React} from 'react';
-import { Navbar } from '../Navbar';
-import Footer from '../Footer';
-
+import { React } from "react";
+import { Navbar } from "../Navbar";
+import Footer from "../Footer";
 
 const Home = () => {
   return (
     <div>
-        <Navbar/>
-          <h1>Hello, Here HomePage</h1>
-        <Footer/>
-      </div>
+      <Navbar />
+      <h1>Hello, Here HomePage</h1>
+      <Footer />
+    </div>
   );
-}
+};
 export default Home;
--- src/components/Vistas/Navbar.jsx
+++ src/components/Vistas/Navbar.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
     return(
-<header class="text-gray-600 body-font">
-  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <Link to="/Home" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <span class="ml-3 text-xl">Smarty</span>
+<header className="text-gray-600 body-font">
+  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <Link to="/Home" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <span className="ml-3 text-xl">Smarty</span>
     </Link>
-    <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <Link to="/Smarty" class="mr-5 hover:text-gray-900">Qué es Smarty</Link>
-      <Link to="/" class="mr-5 hover:text-gray-900">Materias</Link>
-      <Link to="/" class="mr-5 hover:text-gray-900">Registro</Link>
+    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <Link to="/Smarty" className="mr-5 hover:text-gray-900">Qué es Smarty</Link>
+      <Link to="/" className="mr-5 hover:text-gray-900">Materias</Link>
+      <Link to="/" className="mr-5 hover:text-gray-900">Registro</Link>
     </nav>
-    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Inicia Sesión
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Inicia Sesión
+      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
       </svg>
     </button>
   </div>
