31ca1dc71e9c0c81e6fb3fdb0d6bea6504962409
Alba Portillo <portilloalba92@gmail.com>
2023-04-09 03:26:43 -0600

    Update Footer.jsx

--- src/components/Vistas/Footer.jsx
+++ src/components/Vistas/Footer.jsx
@@ -13,16 +13,16 @@ function Footer() {
     <Link className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
     <img className="hidden lg:block h-8 w-auto" src={logo} alt="Smarty logo" />
     </Link>
-    <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 Smarty
+    <p className="text-sm text-blue-700 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 Smarty
     </p>
     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-      <Link className="text-gray-500">
+      <Link className="text-blue-700">
         <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
           <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
         </svg>
       </Link>
 
-      <Link className="ml-3 text-gray-500">
+      <Link className="ml-3 text-blue-700">
         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
           <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
           <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
