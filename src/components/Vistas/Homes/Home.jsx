c5c83d0a1ad98e33a94187c05cb405428640854d
Alba Portillo <portilloalba92@gmail.com>
2023-04-09 21:35:24 -0600

    cambiosssss

--- src/components/Vistas/Homes/Home.jsx
+++ src/components/Vistas/Homes/Home.jsx
@@ -3,6 +3,7 @@ import { Navbar } from "../Navbar";
 import Footer from "../Footer";
 import { Link } from "react-router-dom";
 import imgHome from "../Homes/imgHome.gif";
+import './home.css';
 
 const Home = () => {
   return (
@@ -15,7 +16,7 @@ const Home = () => {
                 <img src={imgHome} alt="" class="object-cover object-center rounded"/>
                   </div>
                   <section className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-                    <h1 className="font-font1 sm:text-4xl text-3xl mb-4 font-medium text-mainblue"> ¡Aprender Siempre es genial!</h1>
+                    <h1 className=" sm:text-3xl text-3xl mb-4 text-blue-600"> ¡Aprender Siempre es genial!</h1>
                       <Link>
                         <span className="bg-blue-100 font-font2 text-blue-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
                           <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="w-3 h-3" viewBox="0 0 24 24">
@@ -53,8 +54,8 @@ const Home = () => {
                                   </Link>
 
                                       <div className="mt-3  p-2 flex justify-center gap-x-6">
-                                          <Link to="/" className="rounded-md bg-blue-600 px-10 py-2.5 text-xl font-font2 text-white shadow-sm hover:bg-mainblue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Registrate</Link>
-                                            <Link to="/" className="rounded-md bg-blue-600 px-6 py-2.5 text-xl font-font2 text-white shadow-sm hover:bg-mainblue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Iniciar sesión</Link>
+                                          <Link to="/Registro" ID="link" className="rounded-md bg-blue-600 px-10 py-2.5 text-xl font-font2 text-white shadow-sm hover:bg-mainblue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Registrate</Link>
+                                            <Link to="/InicioSesion" ID="link"className="rounded-md bg-blue-600 px-6 py-2.5 text-xl font-font2 text-white shadow-sm hover:bg-mainblue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Iniciar sesión</Link>
                                       </div>
                       </section>
                 </div>
