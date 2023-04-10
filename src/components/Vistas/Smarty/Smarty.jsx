92fdb5ce5ad7530ed520edc752f459c91dd431de
Alba Portillo <portilloalba92@gmail.com>
2023-04-09 20:52:56 -0600

    Update Smarty.jsx

--- src/components/Vistas/Smarty/Smarty.jsx
+++ src/components/Vistas/Smarty/Smarty.jsx
@@ -15,11 +15,12 @@ const Smarty = () => {
               <img src={SmartyLearning} alt="" className="object-cover object-center"/>
             </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <p className="title-font sm:text-md text-md mb-4 font-sm text-gray-600"><strong>Smarty</strong> es una aplicación web que te ayudará a fortalecer tus conocimientos de una manera fácil y divertida. Con Smarty, podrás responder cuestionarios de las diferentes materias de ciencias de tu grado y recibir un puntaje por cada uno. Además, podrás personalizar tu perfil con tu foto y ganar insignias según el avance que tengas en los diferentes niveles de cuestionarios y la frecuencia con la que uses la aplicación.
+            <p className="title-font sm:text-md text-md mb-4 font-sm text-gray-600"><strong className='text-xl font-bold text-blue-600'>¿Qué es Smarty?</strong>
+            <br/>Es una aplicación web que te ayudará a fortalecer tus conocimientos de una manera fácil y divertida. Con <strong className='font-bold text-blue-500'>Smarty</strong>, podrás responder cuestionarios de las diferentes materias de ciencias de tu grado y recibir un puntaje por cada uno. Además, podrás personalizar tu perfil con tu foto y ganar insignias según el avance que tengas en los diferentes niveles de cuestionarios y la frecuencia con la que uses la aplicación.
             <br/> <br/>
-            La ventaja de Smarty es que podrás utilizarla desde cualquier dispositivo conectado a internet y en cualquier momento que tengas libre. También podrás ver tus avances y métricas de uso, como el tiempo que pasaste en la aplicación y tu nivel de satisfacción con la misma.
+            La ventaja de <strong className='font-bold text-blue-500'>Smarty</strong> es que podrás utilizarla desde cualquier dispositivo conectado a internet y en cualquier momento que tengas libre. También podrás ver tus avances y métricas de uso, como el tiempo que pasaste en la aplicación y tu nivel de satisfacción con la misma.
             </p>
-              <p className="mb-8 leading-relaxed text-gray-600">¡No pierdas la oportunidad de convertirte en un experto en ciencias con Smarty!</p>
+              <p className="mb-8 leading-relaxed text-gray-600">¡No pierdas la oportunidad de convertirte en un experto en ciencias con <strong className='font-bold text-blue-500'>Smarty</strong>!</p>
 
                 <div className="flex justify-center">
                   <Link to="/" className="rounded-md bg-blue-600 px-6 py-2.5 text-xl font-font2 text-white shadow-sm hover:bg-blue-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Vamos allá</Link>
