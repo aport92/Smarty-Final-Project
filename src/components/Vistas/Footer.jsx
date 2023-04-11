813ee07d24b1d6f6a1ee921b55100cc23b34cdae
Alba Portillo <portilloalba92@gmail.com>
2023-04-10 19:28:56 -0600

    Update Footer.jsx

--- src/components/Vistas/Footer.jsx
+++ src/components/Vistas/Footer.jsx
@@ -1,6 +1,6 @@
 import {React} from 'react';
 import { Link } from 'react-router-dom';
-import logo from "../../images/smarty-home.png";
+import logo from "../../images/smarty-favicon.png";
 import './footer.css';
 function Footer() {
 
