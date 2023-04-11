23fc805b842ab36f6021d8d3f10fe436307999e7
Alba Portillo <portilloalba92@gmail.com>
2023-04-10 23:10:07 -0600

    Update ForgotPass.jsx

--- src/components/Vistas/forgot-password/ForgotPass.jsx
+++ src/components/Vistas/forgot-password/ForgotPass.jsx
@@ -12,7 +12,7 @@ const ForgotPass = () => {
     <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
     <div className="flex flex-col overflow-y-auto md:flex-row">
       <div className="h-32 md:h-auto md:w-1/2">
-      <img src={ForgotPassword} alt="Forgot Password"/></div>
+      <img src={ForgotPassword} alt="Forgot Password" className="object-cover h-full w-full"/></div>
       </div>
       <div className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
         <div className="w-full">
@@ -20,8 +20,7 @@ const ForgotPass = () => {
           <label className="block mb-2">
             <span className="text-gray-700 dark:text-gray-400">Correo electrónico: </span>
             <input className="block w-full mt-1 text-sm font-font3 dark:border-gray-600 dark:bg-gray-700 focus:border-blue-400 focus:outline-none focus:shadow-outline-blue dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-              placeholder="0000000@mail.edu.sv"/>
-          </label>
+              placeholder="0000000@mail.edu.sv"/></label>
 
           <Link className="block w-full px-4 py-2 mt-4 text-sm font-font2 leading-5 text-center text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg hover:bg-blue-800 focus:outline-none focus:shadow-outline-darkblue"
             to="">Recuperar Contraseña</Link>
