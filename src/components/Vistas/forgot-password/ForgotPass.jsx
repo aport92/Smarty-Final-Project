b8fba75d41ee4cd9461c7d2245a855392ea0c411
Alba Portillo <portilloalba92@gmail.com>
2023-04-11 00:03:06 -0600

    cambios

--- src/components/Vistas/forgot-password/ForgotPass.jsx
+++ src/components/Vistas/forgot-password/ForgotPass.jsx
@@ -19,8 +19,7 @@ const ForgotPass = () => {
           <h1 className="block mb-2 text-xl">Olvidé mi contraseña</h1>
           <label className="block mb-2">
             <span className="text-gray-700 dark:text-gray-400">Correo electrónico: </span>
-            <input className="block w-full mt-1 text-sm font-font3 dark:border-gray-600 dark:bg-gray-700 focus:border-blue-400 focus:outline-none focus:shadow-outline-blue dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-              placeholder="0000000@mail.edu.sv"/></label>
+            <input className="block w-full mt-1 text-sm font-font3 dark:border-gray-600 dark:bg-gray-700 focus:border-blue-400 focus:outline-none focus:shadow-outline-blue dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="0000000@mail.edu.sv"/></label>
 
           <Link className="block w-full px-4 py-2 mt-4 text-sm font-font2 leading-5 text-center text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg hover:bg-blue-800 focus:outline-none focus:shadow-outline-darkblue"
             to="">Recuperar Contraseña</Link>
