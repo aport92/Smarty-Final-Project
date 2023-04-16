52a4608320c6ec54ac9d44a643f9ef398a9c4ff1
Alba Portillo <portilloalba92@gmail.com>
2023-04-16 16:22:30 -0600

    changes

--- src/components/Vistas/Materias/Materias.jsx
+++ src/components/Vistas/Materias/Materias.jsx
@@ -3,57 +3,48 @@ import { Navbar } from '../Navbar';
 import Footer from '../Footer';
 import { Link } from 'react-router-dom';
 import '../Materias/materias.css';
-import Math from '../Materias/math.png';
-import Science from '../Materias/science.png';
+import Math from '../Materias/Mathematics.gif';
+import Science from '../Materias/Biologist.gif';
+import Language from '../Materias/Bibliophile.gif';
 
 const Materias = () => {
     return (
 <>
 <Navbar/>
-<div class="container">
-                <div class="col">
-                    <span class="active">
-                        <div class="box">
-                            <i class="fa fa-gift" aria-hidden="true"></i>
-                            <h2>Lorem Ipsum</h2>
-                            <a href="#">Read More</a>
+<div className="container">
+                <div className="col">
+                    <span className="active">
+                        <div className="box">
+                            <i className="fa fa-gift" aria-hidden="true"></i>
+                            <h2>Matemáticas</h2>
+                            <Link to={"#"}>Vamos allá</Link>
                         </div>
                     </span>
-                    <span><img src="https://venture-lab.org/wp-content/uploads/2019/11/writing.jpg"></span>
+                    <span><img src={Math} /></span>
                 </div>
-                <div class="col">
-                    <span><img src="https://i1.wp.com/learnenglishfunway.com/wp-content/uploads/2020/04/expert-tips-for-writing-job-posts.jpg?resize=810%2C500&ssl=1"></span>
-                    <span class="active">
-                        <div class="box">
-                            <i class="fa fa-glass" aria-hidden="true"></i>
-                            <h2>Lorem Ipsum</h2>
-                            <a href="#">Read More</a>
-                        </div>
-                </div>
-                <div class="col">
-                    <span class="active">
-                        <div class="box">
-                            <i class="fa fa-globe" aria-hidden="true"></i>
-                            <h2>Lorem Ipsum</h2>
-                            <a href="#">Read More</a>
-                        </div>
-                    </span>
-                    <span><img src="https://global-exam.com/blog/wp-content/uploads/2018/10/writing-ex1-e1553097551973.jpeg"></span>
-                </div>
-                <div class="col">
-                    <span><img src="https://149349728.v2.pressablecdn.com/wp-content/uploads/2021/03/marcos-paulo-prado-tcyW6Im5Uug-unsplash-scaled-e1615501484783.jpg"></span>
-                    <span class="active">
-                        <div class="box">
-                            <i class="fa fa-graduation-cap" aria-hidden="true"></i>
-                            <h2>Lorem Ipsum</h2>
-                            <a href="#">Read More</a>
+                <div className="col">
+                    <span><img src={Language}/></span>
+                    <span className="active">
+                        <div className="box">
+                            <i className="fa fa-graduation-cap" aria-hidden="true"></i>
+                            <h2>Lenguaje y Literatura</h2>
+                            <Link to={"#"}>Vamos allá</Link>
                         </div>
                     </span>
                 </div>
-            </div>
-            <F
-            ooter/>
-            </>
+                <div className="col">
+                    <span className="active">
+                        <div className="box">
+                            <i className="fa fa-globe" aria-hidden="true"></i>
+                            <h2>Ciencias Naturales</h2>
+                            <Link to={"#"}>Vamos allá</Link>
+                        </div>
+                    </span>
+                    <span><img src={Science} /></span>
+                </div>
+        </div>
+    <Footer/>
+</>
     );
 }
 
