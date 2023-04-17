8c300a926b027065737b916fd01118d18ef47aff
Nancy Ramírez <nancylissethramirezservellon@gmail.com>
2023-04-16 18:24:56 -0600

    l

--- src/components/Vistas/InicioSesion/InicioSesion.jsx
+++ src/components/Vistas/InicioSesion/InicioSesion.jsx
@@ -1,7 +1,7 @@
-import { React } from "react";
+import { React} from "react";
 import { Navbar } from "../Navbar";
 import Footer from "../Footer";
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
 import Aunthentication from "../InicioSesion/Authentication.gif";
 import "../InicioSesion/iniciosesion.css";
 
