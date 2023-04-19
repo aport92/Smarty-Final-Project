1315c2d7e028f1d2a722ccce9efac38c0ac5d7f3
Vanessa Alarcón <vanssa549@gmail.com>
2023-04-19 15:09:00 -0600

    cambio de favicon

--- src/components/Vistas/Quizzes/Quizzesmate/QuizzFraciones.jsx
+++ src/components/Vistas/Quizzes/Quizzesmate/QuizzFraciones.jsx
@@ -1,7 +1,47 @@
+import { ClassNames } from "@emotion/react";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const QuizzFraciones = () => {
 
-}
+    return(
+       
+    <div ClassName="start_btn"><button>Iniciar Quiz</button></div>
+
+   
+
+    <div ClassName="info_box">
+        <div ClassName="info-title"><span> Indicaciones para realizar quiz</span></div>
+        <div ClassName="info-list">
+            <div ClassName="info">1. Tienes sólo <span>15 segundos</span> por cada pregunta.</div>
+            <div ClassName="info">2. Una vez que seleccione su respuesta, no se puede deshacer.</div>
+            <div ClassName="info">3. No puede seleccionar ninguna opción una vez que se acaba el tiempo.</div>
+            <div ClassName="info">4. No puedes salir del Quiz mientras estás jugando.</div>
+            <div ClassName="info">5. Obtendrá puntos en función de sus respuestas correctas.</div>
+        </div>
+        <div ClassName="buttons">
+            <button ClassName="quit">Salir</button>
+            <button ClassName="restart">Continuar</button>
+        </div>
+    </div>
+
+    <div ClassName="quiz_box">
+        <header>
+            <div ClassName="title"> Quiz - Ciencias </div>
+            <div ClassName="timer">
+                <div ClassName="time_left_txt">Tiempo</div>
+                <div ClassName="timer_sec">15</div>
+            </div>
+            <div ClassName="time_line"></div>
+        </header>
+        <section>
+            <div ClassName="que_text">
+               
+            </div>
+            <div ClassName="option_list">
+               
+            </div>
+        </section>
+    );
+};
 export default QuizzFraciones;
