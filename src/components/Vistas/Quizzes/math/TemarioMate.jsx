38563bd633a6519559ddb7d414b21321385a88f1
Vanessa Alarcón <vanssa549@gmail.com>
2023-04-19 11:27:38 -0600

    temario mate

--- src/components/Vistas/Quizzes/math/TemarioMate.jsx
+++ src/components/Vistas/Quizzes/math/TemarioMate.jsx
@@ -2,64 +2,40 @@ import { React } from "react";
 import { Navbar } from "../../Navbar";
 import Footer from "../../Footer";
 import "./temariomate.css";
+import { Link } from "react-router-dom";
+import fraccion from "./que-es-una-fraccion.jpg"
 
 const TemarioMate = () => {
   return (
     <>
       <Navbar />
-      <section class="text-gray-400 bg-gray-900 body-font overflow-hidden">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="-my-8 divide-y-2 divide-gray-800">
-      <div class="py-8 flex flex-wrap md:flex-nowrap">
-        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-          <span class="font-semibold title-font text-white">CATEGORY</span>
-          <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+      <section className="text-gray-400 bg-white body-font overflow-hidden">
+      <div className="py-8 flex  flex-wrap md:flex-nowrap">
+        <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+          <span className="font-semibold title-font text-white">¿Qué es una fracción?</span>
+          <span className="mt-1 text-gray-500 text-sm"></span>
         </div>
-        <div class="md:flex-grow">
-          <h2 class="text-2xl font-medium text-white title-font mb-2">Bitters hashtag waistcoat fashion axe chia unicorn</h2>
-          <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-          <a class="text-blue-400 inline-flex items-center mt-4">Learn More
-            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <div className="md:flex-grow">
+          <h2 className="text-2xl font-medium text-mainblue title-font mb-2">¿Qué es una fracción?</h2>
+          <p className="leading-relaxed">Una fracción representa el número de partes que cogemos de una unidad que está dividida en partes iguales. Se representa por dos números separados por una línea de fracción. 
+           
+          </p>
+          <br></br>
+          <h2 className="text-2xl font-medium text-mainblue title-font mb-2">Términos de una fracción</h2>
+          <p className="leading-relaxed">Los términos de una fracción son el numerador y el denominador. El numerador es el número de partes que tenemos y el denominador es el número de partes en que hemos dividido la unidad.
+Vamos a ver un ejemplo: Tenemos diferentes figuras y cada una de ellas la dividimos en diferentes partes iguales, que es el denominador. La parte coloreada es el numerador.</p>
+          </div>
+          <Link to={"/QuizzFraciones"}className="text-blue-400 inline-flex items-center mt-4">Leer Más
+            <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
               <path d="M12 5l7 7-7 7"></path>
             </svg>
-          </a>
+          </Link>
         </div>
-      </div>
-      <div class="py-8 flex border-t-2 border-gray-800 flex-wrap md:flex-nowrap">
-        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-          <span class="font-semibold title-font text-white">CATEGORY</span>
-          <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
-        </div>
-        <div class="md:flex-grow">
-          <h2 class="text-2xl font-medium text-white title-font mb-2">Meditation bushwick direct trade taxidermy shaman</h2>
-          <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-          <a class="text-blue-400 inline-flex items-center mt-4">Learn More
-            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M5 12h14"></path>
-              <path d="M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
-        </div>
-      </div>
-      <div class="py-8 flex border-t-2 border-gray-800 flex-wrap md:flex-nowrap">
-        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-          <span class="font-semibold title-font text-white">CATEGORY</span>
-          <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
-        </div>
-        <div class="md:flex-grow">
-          <h2 class="text-2xl font-medium text-white title-font mb-2">Woke master cleanse drinking vinegar salvia</h2>
-          <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
-          <a class="text-blue-400 inline-flex items-center mt-4">Learn More
-            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M5 12h14"></path>
-              <path d="M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
+  
+      <div className="h-auto  p-4  rounded-lg overflow-hidden">
+        <img src={fraccion} /></div>
+        
 </section>
        
       <Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx, html}",],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        darkblue: "#244673",
+          mainblue: "#1B62BF",
+          lblue1: "#328BD9",
+          lblue2: "50b4f2",
+          lblue3: "63caf2",
+          dgray: "515151",
+          gray: "787878",
+          mgray: "9d9d9d",
+          lgray1: "c1c1c1",
+          lgray2: " dcdcdc",
+      }
+    },
   },
   plugins: [],
 }
