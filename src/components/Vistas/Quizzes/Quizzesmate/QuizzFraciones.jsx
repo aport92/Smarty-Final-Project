9b80b5cfb83da4472474dbe01cac56b5d5c6f3dd
Vanessa Alarcón <vanssa549@gmail.com>
2023-04-19 15:16:35 -0600

    f

--- src/components/Vistas/Quizzes/Quizzesmate/QuizzFraciones.jsx
+++ src/components/Vistas/Quizzes/Quizzesmate/QuizzFraciones.jsx
@@ -1,47 +1,68 @@
-import { ClassNames } from "@emotion/react";
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
 
 const QuizzFraciones = () => {
+  return (
+    <div>
+        <div className="start_btn"><button>Iniciar Quiz</button></div>
 
-    return(
-       
-    <div ClassName="start_btn"><button>Iniciar Quiz</button></div>
 
-   
-
-    <div ClassName="info_box">
-        <div ClassName="info-title"><span> Indicaciones para realizar quiz</span></div>
-        <div ClassName="info-list">
-            <div ClassName="info">1. Tienes sólo <span>15 segundos</span> por cada pregunta.</div>
-            <div ClassName="info">2. Una vez que seleccione su respuesta, no se puede deshacer.</div>
-            <div ClassName="info">3. No puede seleccionar ninguna opción una vez que se acaba el tiempo.</div>
-            <div ClassName="info">4. No puedes salir del Quiz mientras estás jugando.</div>
-            <div ClassName="info">5. Obtendrá puntos en función de sus respuestas correctas.</div>
-        </div>
-        <div ClassName="buttons">
-            <button ClassName="quit">Salir</button>
-            <button ClassName="restart">Continuar</button>
-        </div>
+<div className="info_box">
+    <div className="info-title"><span> Indicaciones para realizar quiz</span></div>
+    <div className="info-list">
+        <div className="info">1. Tienes sólo <span>15 segundos</span> por cada pregunta.</div>
+        <div className="info">2. Una vez que seleccione su respuesta, no se puede deshacer.</div>
+        <div className="info">3. No puede seleccionar ninguna opción una vez que se acaba el tiempo.</div>
+        <div className="info">4. No puedes salir del Quiz mientras estás jugando.</div>
+        <div className="info">5. Obtendrá puntos en función de sus respuestas correctas.</div>
     </div>
+    <div className="buttons">
+        <button className="quit">Salir</button>
+        <button className="restart">Continuar</button>
+    </div>
+</div>
 
-    <div ClassName="quiz_box">
-        <header>
-            <div ClassName="title"> Quiz - Ciencias </div>
-            <div ClassName="timer">
-                <div ClassName="time_left_txt">Tiempo</div>
-                <div ClassName="timer_sec">15</div>
-            </div>
-            <div ClassName="time_line"></div>
-        </header>
-        <section>
-            <div ClassName="que_text">
-               
-            </div>
-            <div ClassName="option_list">
-               
-            </div>
-        </section>
-    );
+<div className="quiz_box">
+    <header>
+        <div className="title"> Quiz - Ciencias </div>
+        <div className="timer">
+            <div className="time_left_txt">Tiempo</div>
+            <div className="timer_sec">15</div>
+        </div>
+        <div className="time_line"></div>
+    </header>
+    <section>
+        <div className="que_text">
+           
+        </div>
+        <div className="option_list">
+           
+        </div>
+    </section>
+
+  
+    <footer>
+        <div className="total_que">
+           
+        </div>
+        <button className="next_btn">Siguiente</button>
+    </footer>
+</div>
+
+<div className="result_box">
+    <div className="icon">
+        <i className="fas fa-crown"></i>
+    </div>
+    <div className="complete_text">¡Has completado el Quiz!</div>
+    <div className="score_text">
+        
+    </div>
+    <div className="buttons">
+        <button className="restart">Una vez más</button>
+        <button className="quit">Salir </button>
+    </div>
+    </div>
+    </div>
+  );
 };
-export default QuizzFraciones;
+
+export default QuizzFraciones
