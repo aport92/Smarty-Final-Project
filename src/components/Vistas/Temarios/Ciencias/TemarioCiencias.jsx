b2f73a37723d4054a06df53daaf810824fab0953
Vanessa Alarcón <vanssa549@gmail.com>
2023-04-20 10:31:30 -0600

    j

--- src/components/Vistas/Temarios/Ciencias/TemarioCiencias.jsx
+++ src/components/Vistas/Temarios/Ciencias/TemarioCiencias.jsx
@@ -5,71 +5,71 @@ import "./temariociencias.css";
 import { Link } from "react-router-dom";
 import planta from "./planta.webp";
 
-
 const TemarioCiencias = () => {
   return (
     <>
       <Navbar />
       <section className="text-gray-400 bg-white body-font overflow-hidden">
-        
         <div className="py-8 flex  flex-wrap md:flex-nowrap p-20">
           <div className="md:flex-grow">
-            <h2 className="text-2xl font-medium text-mainblue title-font mb-2">Partes de una planta y sus funciones</h2>
-            <p className="leading-relaxed">
-            Todos sabemos identificar de un vistazo las distintas partes de la mayoría de las plantas, como el tronco en un árbol o la raíz de cualquiera de ellas. Sin embargo, ¿conoces bien cuáles son todas las partes de las plantas y qué funciones cumplen?
-
-            <img src={planta} alt="" className="w-1/3 h-1/4 p-6"/>
-
-             <br></br>Puede parecer información básica, pero tanto si lo estás empezando a estudiar ahora como si quieres refrescar tus conocimientos sobre plantas o ampliarlos, te invitamos a acompañarnos en este artículo de EcologíaVerde sobre las 5 partes de una planta y sus funciones con esquemas.  <br></br> Se representa por dos
-              números separados por una línea de fracción.
-            </p>
-            <br></br>
             <h2 className="text-2xl font-medium text-mainblue title-font mb-2">
-              Términos de una fracción
+              Partes de una planta y sus funciones
             </h2>
             <p className="leading-relaxed">
-              Los términos de una fracción son el numerador y el denominador. El
-              numerador es el número de partes que tenemos y el denominador es
-              el número de partes en que hemos dividido la unidad. Vamos a ver
-              un ejemplo: Tenemos diferentes figuras y cada una de ellas la
-              dividimos en diferentes partes iguales, que es el denominador. La
-              parte coloreada es el numerador.
-            </p>
-           
-            <br></br>
-            <h2 className="text-2xl font-medium text-mainblue title-font mb-2">
-              ¿Cómo se leen las fracciones?
-            </h2>
-            <p className="leading-relaxed">
-              El numerador se lee con los números cardenales. 1 – un, 2 – dos, 3
-              – tres, …, 10 – diez, …, 24 – veinticuatro… El denominador se lee
-              con los números partitivos. 2 – medios, 3 – tercios, 4 – cuartos,
-              5 – quintos, 6 – sextos, 7 – séptimos, 8 – octavos, 9 – novenos,
-              10 – décimos. A partir del 11, el número se lee terminado en
-              -avos: 11 – onceavos, 12 – doceavos, …
-            </p>
-            <br></br>
-            <h2 className="text-2xl font-medium text-mainblue title-font mb-2">
-              Tipos de fracciones
-            </h2>
-            <p className="leading-relaxed">
-              <b>Fracción propia:</b> Cuando el numerador es menor que el
-              denominador.
+              Todos sabemos identificar de un vistazo las distintas partes de la
+              mayoría de las plantas, como el tronco en un árbol o la raíz de
+              cualquiera de ellas. Sin embargo, ¿conoces bien cuáles son todas
+              las partes de las plantas y qué funciones cumplen?
+              <img src={planta} alt="" className="w-1/3 h-1/4 p-8 m-8" />
               <br></br>
-              <b>Fracción impropia:</b> Cuando el numerador es mayor que el
-              denominador.
-              <br></br>
-              <b>Fracción unitaria:</b> Cuando el numerador es igual que el
-              denominador.
             </p>
-            
+            <br></br>
+            <h2 className="text-2xl font-medium text-mainblue title-font mb-2"></h2>
+            <p className="leading-relaxed">
+              De forma resumida, las partes importantes de las plantas son estas
+              cinco: Tallo Raíces Hojas Flor Fruto A continuación, explicaremos
+              cada una de estas partes de las plantas y sus funciones.
+            </p>
+
+            <br></br>
+            <h2 className="text-2xl font-medium text-mainblue title-font mb-2">
+              Tallo
+            </h2>
+            <p className="leading-relaxed">
+              El tallo es la parte aérea de la planta que entre sus funciones
+              tiene la de dar soporte y estructura a la misma, sosteniendo sus
+              otros órganos vegetales aéreos, como las hojas y las flores. Otra
+              de sus principales características es que presenta geotropismo
+              negativo, que quiere decir que crece en sentido opuesto a la
+              gravedad. Aunque hemos comentado que es la parte aérea de la
+              planta, lo cierto es que existen varios tipos de tallos y sus
+              funciones: Los tallos aéreos pueden diferenciarse en erguidos,
+              rastreros, trepadores y volubles, aunque hay tallos especiales,
+              como los que presentan espinas, los estolones o los zarcillos.
+            </p>
+            <br></br>
+            <h2 className="text-2xl font-medium text-mainblue title-font mb-2">
+              Tipos de tallos
+            </h2>
+            <p className="leading-relaxed">
+              Los tallos pueden clasificarse de distintas formas, pero la más
+              común de ellas es hacerlo según el medio en que se encuentran, que
+              diferencia tallos subterráneos y aéreos.
+              <b>Los tallos subterráneos</b> se subdividen a su vez en
+              tubérculos, rizomas y bulbos.
+              <br></br>
+              <b>Los tallos aéreos </b> pueden diferenciarse en erguidos,
+              rastreros, trepadores y volubles, aunque hay tallos especiales,
+              como los que presentan espinas, los estolones o los zarcillos.
+              <br> </br>
+            </p>
+
             <div className="py-8 flex  flex-wrap md:flex-nowrap">
-        <Link to={"/quizzciencias"} className="text-mainblue">Relizar Test
-            
-          </Link>
-        </div>
+              <Link to={"/quizzciencias"} className="text-mainblue">
+                Relizar Test
+              </Link>
+            </div>
           </div>
-          
         </div>
       </section>
 
--- src/components/Vistas/Temarios/Lenguaje/TemarioLenguaje.jsx
+++ src/components/Vistas/Temarios/Lenguaje/TemarioLenguaje.jsx
@@ -5,34 +5,51 @@ import "./temariolenguaje.css";
 import { Link } from "react-router-dom";
 import literatura from "./literatura.jpg";
 
-
 const TemarioLenguaje = () => {
   return (
     <>
       <Navbar />
       <section className="text-gray-400 bg-white body-font overflow-hidden">
-        
         <div className="py-8 flex  flex-wrap md:flex-nowrap p-20">
           <div className="md:flex-grow">
-            <h2 className="text-2xl font-medium text-mainblue title-font mb-2">¿Qué es un texto narrativo?</h2>
+            <h2 className="text-2xl font-medium text-mainblue title-font mb-2">
+              ¿Qué es un texto narrativo?
+            </h2>
             <p className="leading-relaxed">
-              Una fracción representa el número de partes que cogemos de una
-              unidad que está dividida en partes iguales. <br></br> Se representa por dos
-              números separados por una línea de fracción.
+              Un texto narrativo es un relato en el que se cuenta una historia
+              real o ficticia que ocurre en un lugar y tiempo concretos.
+              <br></br>
+              En la vida cotidiana, la narración forma parte esencial de nuestra
+              forma de comunicarnos, dado que se trata de una forma de contar
+              una sucesión de hechos en la que un sujeto o grupo de personajes
+              realiza una serie de acciones que tienen un desenlace.
             </p>
             <br></br>
             <h2 className="text-2xl font-medium text-mainblue title-font mb-2">
-              Términos de una fracción
+              ¿Cuáles son los tipos de texto narrativo?
             </h2>
             <p className="leading-relaxed">
-              Los términos de una fracción son el numerador y el denominador. El
-              numerador es el número de partes que tenemos y el denominador es
-              el número de partes en que hemos dividido la unidad. Vamos a ver
-              un ejemplo: Tenemos diferentes figuras y cada una de ellas la
-              dividimos en diferentes partes iguales, que es el denominador. La
-              parte coloreada es el numerador.
+              <b>Cuento:</b> narración breve, con pocos personajes y desenlace rápido.
+              <br></br>
+              <b>Leyenda:</b> narraciones que mezclan hechos reales y sobrenaturales.
+              <br></br>
+              <b>Mito:</b> historia de corte fantástico que explica el origen de un lugar o un suceso.
+              <br></br>
+              <b>Novela:</b> narración real o ficticia, mucho más extensa y compleja que un cuento.
+              <br></br>
+              <b>Poesía épica:</b> narración de hechos legendarios reales o ficticios.
+              <br></br>
+              <b>Crónica:</b> texto que sigue un orden temporal de los hechos para contar una historia.
+              <br></br>
+              <b>Noticia:</b> género periodístico que narra brevemente un suceso actual.
+              <br></br>
+              <b>Reportaje:</b> investigación periodística extensa sobre una persona o hecho.
+              <br></br>
+              <b>Biografía:</b> narración sobre la vida de una persona y sus momentos más relevantes.
+              
+             
             </p>
-            <img src={literatura} alt="" className="w-1/3 h-1/4 p-6"/>
+            <img src={literatura} alt="" className="w-1/3 h-1/4 p-6" />
             <br></br>
             <h2 className="text-2xl font-medium text-mainblue title-font mb-2">
               ¿Cómo se leen las fracciones?
@@ -59,14 +76,13 @@ const TemarioLenguaje = () => {
               <b>Fracción unitaria:</b> Cuando el numerador es igual que el
               denominador.
             </p>
-            
+
             <div className="py-8 flex  flex-wrap md:flex-nowrap">
-        <Link to={"/QuizzLiteratura"} className="text-mainblue">Relizar Test
-            
-          </Link>
-        </div>
+              <Link to={"/QuizzLiteratura"} className="text-mainblue">
+                Relizar Test
+              </Link>
+            </div>
           </div>
-          
         </div>
       </section>
 
