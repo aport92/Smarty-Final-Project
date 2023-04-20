bfb050b5b38b19cb18faefc234b8d9f0a89daa20
Alba Portillo <portilloalba92@gmail.com>
2023-04-19 21:26:17 -0600

    Update QuizzFraciones.jsx

--- src/components/Vistas/Quizzes/Quizzesmate/QuizzFraciones.jsx
+++ src/components/Vistas/Quizzes/Quizzesmate/QuizzFraciones.jsx
@@ -1,6 +1,39 @@
 import React from "react";
 
 const QuizzFraciones = () => {
+  let questions = [
+    {
+      numb: 1,
+      question: "?",
+      answer: "",
+      options: ["", "", "", ""],
+    },
+    {
+      numb: 2,
+      question: "?",
+      answer: "",
+      options: ["", "", "", ""],
+    },
+    {
+      numb: 3,
+      question: "?",
+      answer: "",
+      options: ["", "", "", ""],
+    },
+    {
+      numb: 4,
+      question: "?",
+      answer: "",
+      options: ["", "", "", ""],
+    },
+    {
+      numb: 5,
+      question: "?",
+      answer: "",
+      options: ["", "", "", ""],
+    },
+  ];
+
   return (
     <div>
       <div className="start_btn">
